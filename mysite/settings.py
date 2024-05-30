ac101798c3c5e59c3d9db7436df6c3301f9de6d4
Artem Zolochevskiy <artem.zolochevskiy@gmail.com>
2024-05-30 10:21:01 +0200

    initial Vercel deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.getenv("DB_NAME", os.path.join(BASE_DIR, "db.sqlite3")),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DATABASE"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
     }
 }
 
